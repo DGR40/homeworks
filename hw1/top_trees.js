4113b7d6c63442117b965910c567708ee81e3360
Danny <dgr73@cornell.edu>
2023-02-09 02:58:35 -0500

    test

---
+++ hw1/top_trees.js
@@ -0,0 +1,11 @@
+[
+  "Platanus x hispanica :: Sycamore: London Plane",
+  "Metrosideros excelsa :: New Zealand Xmas Tree",
+  "Lophostemon confertus :: Brisbane Box",
+  "Pittosporum undulatum :: Victorian Box",
+  "Tristaniopsis laurina :: Swamp Myrtle",
+  "Ficus microcarpa nitida 'Green Gem' :: Indian Laurel Fig Tree 'Green Gem'",
+  "Magnolia grandiflora :: Southern Magnolia",
+  "Prunus cerasifera :: Cherry Plum",
+  "Arbutus 'Marina' :: Hybrid Strawberry Tree",
+];
